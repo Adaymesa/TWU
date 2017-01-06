e7a687528f0b29dd29fdd8437c8b2330aae2f8f3
Aday Mesa <adaymoises@MacBook-Air-de-Aday.local>
2017-01-06 11:52:24 +0100

    First exercises

---
+++ exercises.java
@@ -0,0 +1,53 @@
+/**
+ * Created by adaymoises on 04/01/2017.
+ */
+public class exercises {
+    public static void main(String[] args) {
+        printOneAsterisk();
+        printHorizontalLine(8);
+        printOneAsteriskPerLine(3);
+        printTriangle(3);
+        printIsoscelesTriangle();
+    }
+    private static void printOneAsterisk() {
+        System.out.println("*");
+    }
+
+    private static void printHorizontalLine(int asterisks) {
+        for (int i = 0; i <= asterisks; i++) {
+            System.out.print("*");
+        }
+    }
+
+    private static void printOneAsteriskPerLine(int asterisks) {
+        for (int i = 0; i <= asterisks; i++) {
+            System.out.println("*");
+        }
+    }
+
+    private static void printTriangle(int asterisks) {
+        String asteriskLine = "";
+        for (int i = 0; i <= asterisks; i++) {
+            System.out.println(asteriskLine);
+            asteriskLine = asteriskLine.concat("*");
+        }
+    }
+
+      private static void printIsoscelesTriangle() {
+        int i,j,k;
+        for(i=1; i<=3; i++)
+        {
+            for(j=2; j>=i; j--)
+            {
+                System.out.print(" ");
+            }
+            for(k=1; k<=(2*i-1); k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+
